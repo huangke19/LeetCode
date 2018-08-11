632ad09d6a81b9d8251b627c3fc354577469a75d
huangke <huangke@samsource.cn>
2018-08-11 11:32:24 +0800

    最长回文字符串

--- 005. 最长回文子串/005.py
+++ 005. 最长回文子串/005.py
@@ -7,24 +7,21 @@ class Solution:
         """
         s = '#' + '#'.join(s) + '#'
         res = ''
-        t = 0
         n = len(s)
         for i in range(0, n + 1):
-            lenth = min(i + 1, n - i + 1)
-            if lenth < t:
-                continue
-            for j in range(0, lenth):
-                tmpstr = s[i - j: i + j + 1]
-                if self.isPal(tmpstr):
-                    res = tmpstr if len(tmpstr) >= len(res) else res
-                    t = (len(res) - 1) // 2
+            tmp = self.helper(s, i, i)
+            if len(tmp) > len(res):
+                res = tmp
         res = res.replace('#', '')
         print(res)
         return res
 
-    def isPal(self, a):
-        return a == a[::-1]
+    def helper(self, s, l, h):
+        while l >= 0 and h < len(s) and s[l] == s[h]:
+            l -= 1
+            h += 1
+        return s[l + 1:h]
 
-s = 'fuckcuf'
+s = "cwziydanrqvsdtvnnqgjnbrvvwxwqojeqgxhwxdoktjktulemwpbeqscbbtbfvkxsrjetfdrovcrdwzfmnnihtgxybuairswfewvpuscocqifuwylhssldpjrawqdrbvkykpaggspbfrulcktpbofchzikhzxhpocgvdbwpewpywsgqbczmamprklaoovcfecwchhmsaqkhvuvvzjblmgvqpqtnlipgqsanvovylpmxlmxvymppdykphhaamtxjnnlsqfwjwhyywgurteaummwhvavxbcpgrfffxrowluqmqjaugryxdmwvyokdcfcvcytxpixbvwrdgzctejdoaavgtezexmvxgrkpnayvfarkyoruofqmpnsqdzojxqrjsnfwsbzjmaoigytygukqlrcqaxazvmytgfghdczvzphfdbnxtklaiqqsotavdmhiaermluafheowcobjqmrkmlzyas"
 sol = Solution()
 sol.longestPalindrome(s)
